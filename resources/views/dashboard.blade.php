e6e1bb4df24b69e7d0ace4c2de76af32c9136145
ali <ali@alis-MacBook-Air.local>
2025-08-01 20:44:59 +0330

    Pagination

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,9 +27,7 @@
                 @foreach($posts as $post)
                         <div class="flex bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-8">
                             <div class="p-5 flex-1">
-                                <a href="#">
-                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
-                                </a>
+                                <a href="#" class="text-black">Read more</a>
                                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ \Illuminate\Support\Str::words($post->content,20) }}</p>
                                 <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Read more
@@ -43,6 +41,7 @@
                             </a>
                         </div>
                 @endforeach
+                {{ $posts->onEachSide(1)->links() }}
             </div>
         </div>
     </div>
